6fdd9de3f35fb542c65d989d2d94c43ac67ab628
Lorand Szakacs <lsz@lorandszakacs.com>
2019-09-02 16:00:20 +0300

    Add copyright headers

--- aws-core/src/main/scala/busymachines/pureharm/aws/core/anomalies.scala
+++ aws-core/src/main/scala/busymachines/pureharm/aws/core/anomalies.scala
@@ -1,3 +1,20 @@
+/**
+  * Copyright (c) 2017-2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package busymachines.pureharm.aws.core
 
 import busymachines.pureharm.anomaly._
@@ -10,4 +27,4 @@ import busymachines.pureharm.anomaly._
 final case class InvalidAmazonS3RegionIA(r: String) extends InvalidInputAnomaly(s"Invalid AmazonS3 region: $r") {
   override val id: AnomalyID = InvalidAmazonS3RegionID
 }
-case object InvalidAmazonS3RegionID extends AnomalyID { override val name: String = "H_AWS_S3_001" }
+case object InvalidAmazonS3RegionID extends AnomalyID { override val name: String = "PH_AWS_S3_001" }
