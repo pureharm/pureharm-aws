7776f70e6624f897085a475d0f5a480f8ffe9b27
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-15 12:46:48 +0200

    [s3] Add Java object to S3Metadata to expose information, until we properly parse it and categorize it

--- aws-s3/src/main/scala/busymachines/pureharm/aws/s3/S3Metadata.scala
+++ aws-s3/src/main/scala/busymachines/pureharm/aws/s3/S3Metadata.scala
@@ -1,5 +1,17 @@
 package busymachines.pureharm.aws.s3
 
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse
+
+/** @param contentLength
+  * Neatly parsed and categorized metadata
+  *
+  * @param javaMetadata
+  * All metadata returned by the Java SDK,
+  * in time this will be phased out, as we
+  * add more and more properly structured data
+  * to this case class.
+  */
 final case class S3Metadata(
-  contentLength: S3ContentLengthBytes
+  contentLength: S3ContentLengthBytes,
+  javaMetadata:  HeadObjectResponse,
 )
